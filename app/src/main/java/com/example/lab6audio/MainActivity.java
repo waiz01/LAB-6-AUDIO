e1ff44e7bdc94ee1d4290f06f4f8e525a24b6874
waiz01 <2020628062@student.uitm.edu.my>
2021-12-11 11:08:03 +0800

    LAB6 UWU

---
+++ app/src/main/java/com/example/lab6audio/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lab6audio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = MediaPlayer.create(this, R.raw.song);
+    }
+
+
+    public void playSong(View v) {
+        mp.start();
+    }
+    public void pauseSong(View v) {
+        mp.pause();
+    }
+
+    public void stopSong(View v) {
+        mp.stop();
+        mp=MediaPlayer.create(this, R.raw.song);
+    }
+
+}
